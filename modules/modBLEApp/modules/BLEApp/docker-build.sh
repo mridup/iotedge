f4d1587b23f2d3152109db12cdc7cb448bc2ce53
mridu <mridupawan.das@st.com>
2018-11-14 03:17:28 -1000

    docker build-push cmd for modbleapp

--- modules/modBLEApp/modules/BLEApp/docker-build.sh
+++ modules/modBLEApp/modules/BLEApp/docker-build.sh
@@ -1,1 +1,1 @@
-docker build  --rm -f ./Dockerfile.arm32v7 -t edgemodules.azurecr.io/modappmodule:0.0.1-arm32v7 . && docker push edgemodules.azurecr.io/modappmodule:0.0.1-arm32v7
+docker build  --rm -f ./Dockerfile.arm32v7 -t edgemodules.azurecr.io/modbleapp:0.0.1-arm32v7 . && docker push edgemodules.azurecr.io/modbleapp:0.0.1-arm32v7
